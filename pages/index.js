92976b3e3c93bec956ce1a16cd3c63eaf2da355d
Binaya Maharjan <binaya@eventlogic.se>
2022-08-26 11:37:05 +0545

    AppcuesSettings

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Head from "next/head";
+import Script from 'next/script'
 import CustomComponents from "../components/custom/Custom-components";
 
 export default function Home() {
    return (
       <div>
+         <Script src="//fast.appcues.com/122182.js" />
+         <Script id="show-banner">
+            {`window.AppcuesSettings = {
+         enableURLDetection: true
+      };`}
+         </Script>
+         <script ></script>
          <Head>
             <title>Binay Maharjan - A Software Engineer</title>
             <meta
